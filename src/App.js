4bd4a97147268c77bddaab3f494a27b209c0264f
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2019-11-04 03:46:34 +0500

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(5);
+  const [condition, setCondition] = useState(true);
+  const [mounted, setMounted] = useState(0);
+  const [unmounted, setUnmounted] = useState(0);
+
+  useEffect(() => {
+    alert("This Occurs only on ComponentDidMount");
+    return () => alert("This Occurs on ComponentWillUnMount");
+  }, []);
+
+  useEffect(() => {
+    setMounted(count);
+    return () => setUnmounted(count);
+  }, [condition, count]);
 
   return (
     <div className="App">
@@ -11,6 +24,13 @@ function App() {
       <h3> Count : {count} </h3>
       <button onClick={() => setCount(count + 1)}>Inc +</button>
       <button onClick={() => setCount(count - 1)}>Dec -</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setCondition(!condition)}>
+        Click Me to see changes!
+      </button>
+      <p>ComponentDidMount/Update ===>>> : {mounted}</p>
+      <p>ComponentWillUnmount ===>>> : {unmounted}</p>
+      <p></p>
     </div>
   );
 }
